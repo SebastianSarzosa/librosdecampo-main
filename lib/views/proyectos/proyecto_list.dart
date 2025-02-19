8b246363362854a1069a53185a678870af841dcc
LizzyJoss <joselinespin01@gmail.com>
2025-02-18 21:14:46 -0500

    buscador

--- lib/views/proyectos/proyecto_list.dart
+++ lib/views/proyectos/proyecto_list.dart
@@ -3,25 +3,55 @@ import 'package:libroscampo/models/proyectos.dart';
 import 'package:libroscampo/repositories/proyectos_repository.dart';
 import 'package:libroscampo/views/plantas/planta_list.dart';
 
-class ProyectoListView extends StatelessWidget {
+class ProyectoListView extends StatefulWidget {
   final int libroId;
   final String libroNombre;
   final String userRole;  // Asegúrate de que este campo sea final
   final String userName; // Añade el campo userName
 
-  ProyectoListView({required this.libroId, required this.libroNombre, required this.userRole,required this.userName});
+  ProyectoListView({required this.libroId, required this.libroNombre, required this.userRole, required this.userName});
+
+  @override
+  _ProyectoListViewState createState() => _ProyectoListViewState();
+}
+
+class _ProyectoListViewState extends State<ProyectoListView> {
+  final ProyectosRepository proyectosRepository = ProyectosRepository();
+  List<Proyecto> proyectosList = [];
+  List<Proyecto> filteredProyectosList = [];
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProyectos();
+    searchController.addListener(_filterProyectos);
+  }
+
+  void _fetchProyectos() async {
+    final proyectos = await proyectosRepository.listProjectsByBook(widget.libroId);
+    setState(() {
+      proyectosList = proyectos;
+      filteredProyectosList = proyectos;
+    });
+  }
+
+  void _filterProyectos() {
+    final query = searchController.text.toLowerCase();
+    setState(() {
+      filteredProyectosList = proyectosList.where((proyecto) {
+        return proyecto.nombreProyecto.toLowerCase().contains(query) ||
+               (proyecto.descripcion?.toLowerCase().contains(query) ?? false);
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-  print('User  Role: $userRole'); 
-    final ProyectosRepository proyectosRepository = ProyectosRepository();
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("Proyectos de $libroNombre",
-           style: TextStyle(color: Colors.white, 
-           fontSize: 19.5,
-           ),       
+        title: Text("Proyectos de ${widget.libroNombre}",
+          style: TextStyle(color: Colors.white, fontSize: 19.5),
         ),
         foregroundColor: Colors.white,
         backgroundColor: Colors.green,
@@ -29,82 +59,98 @@ class ProyectoListView extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pushReplacementNamed(context, '/libro/index', 
-            arguments: {'userRole': userRole, 'userName': userName}); // Regresar a la pantalla de libros
+            arguments: {'userRole': widget.userRole, 'userName': widget.userName}); // Regresar a la pantalla de libros
           },
         ),
       ),
-      
-      body: FutureBuilder<List<Proyecto>>(
-        future: proyectosRepository.listProjectsByBook(libroId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.red)));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No hay proyectos disponibles', style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic)));
-          }
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                labelText: 'Buscar proyectos',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<List<Proyecto>>(
+              future: proyectosRepository.listProjectsByBook(widget.libroId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.red)));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No hay proyectos disponibles', style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic)));
+                }
 
-          final proyectos = snapshot.data!;
+                final proyectos = filteredProyectosList;
 
-          return ListView.builder(
-            padding: const EdgeInsets.all(8.0), // Padding para que no esté pegado al borde
-            itemCount: proyectos.length,
-            itemBuilder: (context, index) {
-              final proyecto = proyectos[index];
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(vertical: 8.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12), // Bordes redondeados
-                ),
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(16.0), // Padding dentro de cada card
-                  leading: Icon(Icons.book, size: 40, color: Colors.teal), // Ícono en el inicio de cada item
-                  title: Text(
-                    proyecto.nombreProyecto,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.teal,
-                    ),
-                  ),
-                  subtitle: Text(
-                    'Descripción: ${proyecto.descripcion}',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey[600], // Color gris para la descripción
-                    ),
-                  ),
-                  trailing: Icon(Icons.arrow_forward_ios, color: Colors.teal), // Ícono de flecha al final
-                  onTap: () {
-                      // Navegar a la pantalla de proyectos
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PlantaListView(
-                            proyectoId: proyecto.idProyecto!, proyectoNombre: proyecto.nombreProyecto, userRole: userRole, userName: userName, libroId: libroId, libroNombre: libroNombre
+                return ListView.builder(
+                  padding: const EdgeInsets.all(8.0), // Padding para que no esté pegado al borde
+                  itemCount: proyectos.length,
+                  itemBuilder: (context, index) {
+                    final proyecto = proyectos[index];
+                    return Card(
+                      elevation: 5,
+                      margin: EdgeInsets.symmetric(vertical: 8.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12), // Bordes redondeados
+                      ),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(16.0), // Padding dentro de cada card
+                        leading: Icon(Icons.book, size: 40, color: Colors.teal), // Ícono en el inicio de cada item
+                        title: Text(
+                          proyecto.nombreProyecto,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.teal,
                           ),
                         ),
-                      );
-                    },
-                ),
-              );
-            },
-          );
-        },
+                        subtitle: Text(
+                          'Descripción: ${proyecto.descripcion}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[600], // Color gris para la descripción
+                          ),
+                        ),
+                        trailing: Icon(Icons.arrow_forward_ios, color: Colors.teal), // Ícono de flecha al final
+                        onTap: () {
+                          // Navegar a la pantalla de proyectos
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PlantaListView(
+                                proyectoId: proyecto.idProyecto!, proyectoNombre: proyecto.nombreProyecto, userRole: widget.userRole, userName: widget.userName, libroId: widget.libroId, libroNombre: widget.libroNombre
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: userRole == 'admin' // Mostrar el botón solo si el rol es admin
+      floatingActionButton: widget.userRole == 'admin' // Mostrar el botón solo si el rol es admin
       ? FloatingActionButton(
           onPressed: () {
             Navigator.pushNamed(
               context,
               '/proyecto/form',
               arguments: {
-                'libroId': libroId, 
-                'libroNombre': libroNombre, 
-                'userRole': userRole, 
-                'userName': userName
+                'libroId': widget.libroId, 
+                'libroNombre': widget.libroNombre, 
+                'userRole': widget.userRole, 
+                'userName': widget.userName
               },
             );
           },
@@ -117,5 +163,4 @@ class ProyectoListView extends StatelessWidget {
       : null, // Si no es admin, no mostrar el botón
     );
   }
-  
 }
